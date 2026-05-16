a520269af3877620325c832d1128874850168e50
balli <nursimabetulballi@gmail.com>
2026-05-16 23:27:08 +0300

    ascii kontrolu ve dosya boyutu hesaplama eklendi

--- src/tarsau.c
+++ src/tarsau.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+
+#define MAX_DOSYA_SAYISI 32
+#define MAX_DOSYA_BOYUTU (200 * 1024 * 1024)
+
+int ascii_kontrol(const char *dosya_adi);
+long dosya_boyutu_al(const char *dosya_adi);
+
 int main(int argc, char *argv[])
 {
     if(argc < 2)
@@ -43,12 +52,31 @@ int main(int argc, char *argv[])
         }
 
         printf("Birlestirilecek Dosyalar:\n");
+        long toplam_boyut = 0;
 
         for(int i = 0; i < dosya_sayisi; i++)
         {
             if(access(dosyalar[i], F_OK) == 0)
             {
                 printf("  %s bulundu.\n", dosyalar[i]);
+
+                if(!ascii_kontrol(dosyalar[i]))
+                {
+                    printf("%s giris dosyasinin formati uyumsuz!\n", dosyalar[i]);
+                    return 1;
+                }
+
+                long boyut = dosya_boyutu_al(dosyalar[i]);
+
+                if(boyut < 0)
+                {
+                    printf("%s okunamadi!\n", dosyalar[i]);
+                    return 1;
+                }
+
+                toplam_boyut += boyut;
+
+                printf("  Boyut: %ld byte\n", boyut);
             }
             else
             {
@@ -57,6 +85,16 @@ int main(int argc, char *argv[])
         }
 
         printf("Toplam dosya sayisi: %d\n", dosya_sayisi);
+        printf("Toplam boyut: %ld byte\n", toplam_boyut);
+
+        if(toplam_boyut > MAX_DOSYA_BOYUTU)
+        {
+            printf("Toplam dosya boyutu 200 MB'i gecemez!\n");
+            return 1;
+        }
+        
+        
+        
     }
 
     // Acma Islemi
@@ -81,4 +119,43 @@ int main(int argc, char *argv[])
     }
 
     return 0;
+}
+
+
+int ascii_kontrol(const char *dosya_adi)// Dosya türü text ise 1 değilse 0 döndürülecek
+{
+    FILE *fp = fopen(dosya_adi, "rb");
+
+    if(fp == NULL)
+    {
+        return 0;
+    }
+
+    int ch;
+
+    while((ch = fgetc(fp)) != EOF)
+    {
+        if(ch > 127)
+        {
+            fclose(fp);
+            return 0;
+        }
+    }
+
+    fclose(fp);
+
+    return 1;
+}
+
+
+long dosya_boyutu_al(const char *dosya_adi)
+{
+    struct stat st;
+
+    if(stat(dosya_adi, &st) != 0)
+    {
+        return -1;
+    }
+
+    return st.st_size;
 }
